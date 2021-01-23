36ed1b4fee45261aa8aba2d95e0fa2cb3b255198
liaowenduo <soltuwork@163.com>
2021-01-23 13:46:37 +0800

    lodash-array-meanBy

--- lodash-demo/math/meanBy.js
+++ lodash-demo/math/meanBy.js
@@ -6,3 +6,16 @@ let temp = meanBy(arr, (ele) => {
     return ele * 10;
 })
 console.log(temp); // 18
+
+let arr2 = [{
+    age: 22
+}, {
+    age :40
+}, {
+    age: 23
+}];
+
+temp = meanBy(arr2, (ele) => {
+    return ele.age;
+});
+console.log(temp); // 28.333
