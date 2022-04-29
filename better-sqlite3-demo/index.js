0e5c7992a5a510dab290c688845a3a42bf2d473f
liaowenduo <liaowenduo@didiglobal.com>
2022-04-29 17:50:57 +0800

    feat: better-sqlite3

--- better-sqlite3-demo/index.js
+++ better-sqlite3-demo/index.js
@@ -31,8 +31,33 @@ insertMany([
 
 const stmt = db.prepare("SELECT name, age FROM cats");
 
-let re = db.prepare("SELECT * FROM cats").run();
+let re = db.prepare("SELECT * FROM cats").all();
 console.log(re);
+
+db.pragma("cache_size = 32000");
+console.log(db.pragma("cache_size", { simple: true })); // => 32000
+
+db.backup(path.resolve(__dirname, "foorbar_backup.db")).then(() => {
+  console.log("c");
+});
+
+db.table("sequence", {
+  columns: ["value"],
+  parameters: ["length", "start"],
+  rows: function* (length, start = 0) {
+    if (length === undefined) {
+      throw new TypeError('missing required parameter "length"');
+    }
+
+    const end = start + length;
+    for (let n = start; n < end; ++n) {
+      yield { value: n };
+    }
+  },
+});
+
+db.prepare("SELECT * FROM sequence(10)").pluck().all();
+// => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // 婚姻，事业，孩子，健康，
 // 好人，大人(血性)，能人，名人，伟人
