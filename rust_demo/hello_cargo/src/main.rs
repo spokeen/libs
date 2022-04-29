83a763a6af9796860dd0d5ce7eab152c3e91d825
liaowenduo <liaowenduo@didiglobal.com>
2022-04-29 17:51:19 +0800

    feat: rust

--- rust_demo/hello_cargo/src/main.rs
+++ rust_demo/hello_cargo/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    takes_and_gives_back(s2);  
+
+    println!("{}", s2);
+}
+
+
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+    // scope
+
+a_string  // a_string is returned and moves out to the calling function
 }
